dd8db64d8be67753afc98b04fa8befa7e8f2dd5d
Albertleungwyman <85030219+KAIFENG-ch@users.noreply.github.com>
2022-06-17 20:59:46 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,29 +1,5 @@
-Xshell 7 (Build 0109)
-Copyright (c) 2020 NetSarang Computer, Inc. All rights reserved.
-
-Type `help' to learn how to use Xshell prompt.
-[C:\~]$ 
-
-Connecting to 120.78.12.129:22...
-Connection established.
-To escape to local shell, press 'Ctrl+Alt+]'.
-
-WARNING! The remote SSH server rejected X11 forwarding request.
-
-Welcome to Alibaba Cloud Elastic Compute Service !
-
-Updates Information Summary: available
-    20 Security notice(s)
-         1 Critical Security notice(s)
-         6 Important Security notice(s)
-        12 Moderate Security notice(s)
-         1 Low Security notice(s)
-Run "dnf upgrade-minimal --security" to apply all updates.
-Last login: Fri Jun 17 17:43:03 2022 from 112.49.214.226
-[root@Albert ~]# cd shell-demo/
-[root@Albert shell-demo]# vim test.sh 
-
 #! /bin/bash 
+
 choice() { 
         echo " 
         ---系统信息获取和展示--- 
